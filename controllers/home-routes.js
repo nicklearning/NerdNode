49c6461c9008f90a41be28f215498cf2ab6d310d
Nick Smith <nick.smith.asu@gmail.com>
2024-02-21 13:09:10 -0700

    Add login form

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -14,7 +14,7 @@ router.get('/', async (req, res) => {
         // Serialize the blogposts to pass them to Handlebars
         const blogPosts = blogPostData.map((post) => post.get({ plain: true }));
 
-   
+
         // Pass the serialized blog posts data and the logged_in status to the view
         res.render('home', {
             blogPosts,
@@ -26,6 +26,15 @@ router.get('/', async (req, res) => {
         console.log(err);
         res.status(500).json(err)
     }
+});
+
+router.get('/login', (req, res) => {
+    // If a session exists, redirect the request to the homepage
+    if (req.session.logged_in) {
+        res.redirect('/');
+        return;
+    }
+    res.render('login');
 });
 
 module.exports = router;
---
+++ public/js/login.js
@@ -0,0 +1,29 @@
+const loginFormHandler = async (event) => {
+    // Stop the browser from submitting the form so we can do so with JavaScript
+    event.preventDefault();
+
+    console.log('submit fired!!');
+
+    // Gather the data from the form elements on the page
+    const username = document.querySelector('#username').value.trim();
+    const password = document.querySelector('#password').value.trim();
+
+    if (username && password) {
+        // Send the username and password to the server
+        const response = await fetch('/api/users/login', {
+            method: 'POST',
+            body: JSON.stringify({ username, password }),
+            headers: { 'Content-Type': 'application/json' },
+        });
+
+        if (response.ok) {
+            document.location.replace('/');
+        } else {
+            alert('Failed to log in');
+        }
+    }
+};
+
+document
+    .querySelector('.login-form')
+    .addEventListener('submit', loginFormHandler);
